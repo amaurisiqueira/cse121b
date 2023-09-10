ed784172a648b04d76203a82e11d3f2d72b12f98
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-09-10 18:59:15 -0300

    task about variables

---
+++ W1/activity1/debugging.js
@@ -0,0 +1,21 @@
+const PI= 3.14;
+/*let myVar=3;
+
+
+
+console.log(PI);
+const radius=3;
+area = radius * radius * PI;
+
+//radius = 4;
+area= radius * radius * PI;
+
+console.log(radius);
+console.log(area);
+*/
+function circleArea(radius) {
+    // code to complete our task here
+    return  radius * radius * PI;
+}
+area =  circleArea(3);
+console.log(area);
