256257535eec34f20cdef1b49151a49f3ad1f2a4
amaurisiqueira <amauri.ferreira.siqeuira@gmail.com>
2023-09-17 18:55:07 -0300

    add folder

---
+++ scripts/w02-task.js
@@ -0,0 +1,53 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName ="Amauri Siqueira";
+let currentYear = new Date().getFullYear();
+
+let profilePicture ="images/photo.jpg";
+/* Step 3 - Element Variables */
+
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement  = document.getElementById('year');
+const imageElement = document.getElementById('image');
+
+/* Step 4 - Adding Content */
+
+
+nameElement.innerHTML =  `<strong>${fullName}</strong>`;
+year.textContent=currentYear;
+console.log(profilePicture);
+console.log(imageElement);
+
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt',`Profile image of ${fullName}`);
+ 
+
+
+/* Step 5 - Array */
+
+let myFavoriteFood = ['Pasta','Pizza','Lasagna','Steak'];
+    
+foodElement.innerHTML += `${myFavoriteFood}`;
+
+    let anotherFavoriteFood=['Salad'];
+    myFavoriteFood.push(anotherFavoriteFood);
+
+ foodElement.innerHTML += `<br>${myFavoriteFood}`;
+
+    myFavoriteFood.shift();
+    myFavoriteFood.push('Rice');
+
+foodElement.innerHTML += `<br>${myFavoriteFood}`;
+    myFavoriteFood.pop();
+foodElement.innerHTML += `<br>${myFavoriteFood}`;
+
+
+
+
+
+
